d7b6deb13c0066f9a5885f522f798516d3a16c4a
Shivam <shivam.chem21@iitg.ac.in>
2024-08-24 19:51:44 +0530

    implement initial file and folder structure

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,12 @@
+import { signUp } from "@/lib/actions/user.actions";
+import React from "react";
+
+const SignUp = () => {
+    return (
+        <div>
+            SignUp
+        </div>
+    )
+}
+
+export default signUp
